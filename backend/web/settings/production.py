1a10ea864bfd673c6ccc505a43fa45b4b3a3c2ad
Asamatter <jph8041@gmail.com>
2024-01-09 16:47:44 -0500

    update files

--- backend/web/settings/production.py
+++ backend/web/settings/production.py
@@ -1,8 +1,14 @@
 from .base import *
+import dj_database_url
+
 
 DEBUG = False
 
 STATIC_ROOT = BASE_DIR / "static"
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+DATABASES = {'default': dj_database_url.config(default=DATABASE_URL, engine='django.db.backends.postgresql')}
 
 DATABASES = {
         'default': {            
